919384eef1c54ae2304c334e2f75ba29f82fa8a4
Sahil Shefeek <sahilms345@gmail.com>
2023-09-13 19:22:19 +0530

    added greeting for users

--- django_guacamole/hello/urls.py
+++ django_guacamole/hello/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.hello,name = 'hello'),
-    path("<str:name>",views.greetUser,name = 'greet')
+    path("",views.hello,name = "hello"),
+    path("<str:name>",views.greet_user,name = "greet"),
+    path("greet/<str:username>",views.welcome_user,name = "welcome"),
 ]
--- django_guacamole/hello/views.py
+++ django_guacamole/hello/views.py
@@ -4,5 +4,10 @@ from django.shortcuts import render
 def hello(request):
     return HttpResponse("Hello world!")
 
-def greetUser(request, name):
+def greet_user(request, name):
     return HttpResponse(f"Hello, {name.capitalize()}")
+
+def welcome_user(request, username):
+    return render(request,"hello/index.html",{
+        "username": username,
+    })
