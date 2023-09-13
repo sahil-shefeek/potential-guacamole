48098f138ecc4124c438443317a38cf5b7cb6fc0
Sahil Shefeek <sahilms345@gmail.com>
2023-09-13 18:22:21 +0530

    added dynamic url in hello

--- django_guacamole/hello/views.py
+++ django_guacamole/hello/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def hello(request):
     return HttpResponse("Hello world!")
+
+def greetUser(request, name):
+    return HttpResponse(f"Hello, {name.capitalize()}")
